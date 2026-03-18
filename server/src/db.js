91f4f8e8b6bd883df926bf108220e9c7a6e34d65
StephenOnyeka <ejeanobionyeka@gmail.com>
2026-03-18 21:56:43 +0100

    successfully resolved connection to mongodb issue

--- server/src/db.js
+++ server/src/db.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first'); // Fix for Node 18+ querySrv ECONNREFUSED on some networks
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Bypass local router DNS that blocks SRV queries
+
 
 const connectDB = async () => {
     try {
@@ -7,8 +11,14 @@ const connectDB = async () => {
             throw new Error('MONGODB_URI environment variable is not defined');
         }
 
-        await mongoose.connect(uri);
-        console.log('MongoDB Connected Successfully');
+        mongoose.connection.on('connecting', () => console.log('Mongoose is connecting...'));
+        mongoose.connection.on('connected', () => console.log('Mongoose connected successfully!'));
+        mongoose.connection.on('error', (err) => console.error('Mongoose encountered an error:', err.message));
+        mongoose.connection.on('disconnected', () => console.log('Mongoose is disconnected.'));
+
+        await mongoose.connect(uri, {
+            serverSelectionTimeoutMS: 15000
+        });
     } catch (error) {
         console.error('MongoDB Connection Error:', error.message);
         process.exit(1);
